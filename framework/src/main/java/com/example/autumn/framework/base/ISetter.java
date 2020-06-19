6c57d887fb81f1996589cfc45acdeac6229a9f62
yanlianglong <709248556@qq.com>
2020-06-19 15:05:34 +0800

    工具类

---
+++ framework/src/main/java/com/example/autumn/framework/base/ISetter.java
@@ -0,0 +1,9 @@
+package com.example.autumn.framework.base;
+
+/**
+ * @description:
+ * @author: yanlianglong
+ * @create: 2020-06-19 11:55
+ **/
+public interface ISetter {
+}
--- framework/src/main/java/com/example/autumn/framework/test/ReflectionUtil.java
+++ framework/src/main/java/com/example/autumn/framework/test/ReflectionUtil.java
@@ -3,15 +3,18 @@ package com.example.autumn.framework.test;
 import org.apache.commons.lang.StringUtils;
 
 import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-public class ReflectionFieldName {
+public class ReflectionUtil {
+    /***
+    * @Description: 获取对应的字段名
+    * @Param: [func]
+    */
     public static <T> String getFieldName(PropertyFunc<T, ?> func) {
         try {
-            // 通过获取对象方法，判断是否存在该方法
             Method method = func.getClass().getDeclaredMethod("writeReplace");
             method.setAccessible(Boolean.TRUE);
-            // 利用jdk的SerializedLambda 解析方法引用
             SerializedLambda serializedLambda = (SerializedLambda) method.invoke(func);
             String getter = serializedLambda.getImplMethodName();
             return resolveFieldName(getter);
@@ -26,7 +29,6 @@ public class ReflectionFieldName {
         } else if (getMethodName.startsWith("is")) {
             getMethodName = getMethodName.substring(2);
         }
-        // 小写第一个字母
         return firstToLowerCase(getMethodName);
     }
 
@@ -35,5 +37,27 @@ public class ReflectionFieldName {
             return "";
         }
         return param.substring(0, 1).toLowerCase() + param.substring(1);
+    }
+
+    /***
+    * @Description: 获取对应的字段值
+    * @Param: [object, func]
+    */
+    public static <T> Object getFieldValue(Object object,PropertyFunc<T, ?> func) {
+        return getValue(object,getFieldName(func));
+    }
+    private static Object getValue(Object object, String name){
+        Object value = null;
+        try {
+            Class<?> clz = object.getClass();
+            Field nameField = clz.getDeclaredField(name);
+            nameField.setAccessible(true);
+            value = nameField.get(object);
+        }catch (NoSuchFieldException e){
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return value;
     }
 }
